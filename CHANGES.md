b8a7bcd0cb66a4ae6e9844d1acf93effbae4e3fe
Jan Thomä <janthomae@janthomae.de>
2024-12-02 07:07:46 +0100

    chore: cleanup/prepare release

--- CHANGES.md
+++ CHANGES.md
@@ -4,12 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [0.0.2] - 2024-12-02
 ### Breaking Changes
 - Removed the leftover`get_value_axis_xxx` methods from the `GUIDEAction` class. Use the `value_axis_xxx` property instead.
-- `GUIDEDebugger` is no longer a public node class because it cannot work as a standalone node. 
+- `GUIDEDebugger` is no longer a public node class because it cannot work as a standalone node. Use the provided `guide_debugger.tscn` scene to add a debugger to your project.
 ### Added
 - `GUIDEAction` now has additional functions to check whether it is currently in `Completed` or `Ongoing` state.
+- Added two new modifiers: _Curve_ which allows to apply a curve to the input value and _Map range_ which allows to map the input value to a different range. A huge thanks goes to [Regal Media](https://github.com/RegalMedia) for submitting a PR with this feature!
 ### Improved
 - The `input_mappings_changed` signal in `GUIDE` now also fires when any joystick is connected or disconnected.
 
--- addons/guide/modifiers/guide_modifier_curve.gd
+++ addons/guide/modifiers/guide_modifier_curve.gd
@@ -5,16 +5,16 @@ extends GUIDEModifier
 
 
 ## The curve to apply to the x axis
-@export var curve:Curve
+@export var curve: Curve = default_curve()
 
 ## Apply modifier to X axis
-@export var x:bool = true
+@export var x: bool = true
 
 ## Apply modifier to Y axis
-@export var y:bool = true
+@export var y: bool = true
 
 ## Apply modifier to Z axis
-@export var z:bool = true
+@export var z: bool = true
 
 
 ## Create default curve resource with a smoothstep, 0.0 - 1.0 input/output range
@@ -26,26 +26,17 @@ static func default_curve() -> Curve:
 	return curve
 
 
-# Initialize this resource with default curves
-func _init(_curve:Curve = null, _x:bool = true, _y:bool = false, _z:bool = false):
-	if _curve == null:
-		_curve = default_curve()
-
-	curve = _curve
-
-
-func _modify_input(input:Vector3, delta:float, value_type:GUIDEAction.GUIDEActionValueType) -> Vector3:
+func _modify_input(input: Vector3, delta: float, value_type: GUIDEAction.GUIDEActionValueType) -> Vector3:
 	# Curve should never be null
-	assert(curve != null, "No curve added to Curve modifier")
+	if curve == null:
+		push_error("No curve added to Curve modifier.")
+		return input
 
-	var x_value:float = curve.sample(input.x)
-	var y_value:float = curve.sample(input.y)
-	var z_value:float = curve.sample(input.z)
-
+	# Return vector with enabled axes modified, others remain unchanged.
 	return Vector3(
-		x_value if x else input.x,
-		y_value if y else input.y,
-		z_value if z else input.z
+		curve.sample(input.x) if x else input.x,
+		curve.sample(input.y) if y else input.y,
+		curve.sample(input.z) if z else input.z
 	)
 
 
@@ -54,4 +45,4 @@ func _editor_name() -> String:
 
 
 func _editor_description() -> String:
-	return "Applies a separate curve to each input axis."
+	return "Applies a curve to each input axis."
