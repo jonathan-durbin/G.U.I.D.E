702d454d3f6c9deee5c2ed7cd930f465b6075664
Jan Thomä <janthomae@janthomae.de>
2025-07-31 09:04:46 +0200

    feat: add junie guidelines

---
+++ run_tests.cmd
@@ -0,0 +1,11 @@
+﻿@echo off
+setlocal
+
+REM Check if any arguments were provided
+if "%~1"=="" (
+    REM No arguments provided, use default
+    addons\gdUnit4\runtest.cmd --godot_bin c:\tools\godot\4.2\godot42.exe -a tests
+) else (
+    REM Arguments provided, pass them to the test runner
+    addons\gdUnit4\runtest.cmd --godot_bin c:\tools\godot\4.2\godot42.exe %*
+)
